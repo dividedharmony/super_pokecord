82fd1dda53d74899c2ebdb8635dd098b0b95d23a
David Harmon <dharmon202@gmail.com>
2020-06-05 09:03:59 -0400

    Allow wild pokemon to appear at set intervals
    This is a feature where Pokemon will appear after a set number of
    non-command messages have been set in specific pokecord channels.

--- main.rb
+++ main.rb
@@ -6,4 +6,18 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new(token: ENV["DISCORD_TOKEN"])
 
+poke_channels = (ENV["POKECORD_CHANNELS"] || "").split(',')
+poke_steps = 0
+poke_requirement = 3
+
+bot.message(containing: not!("p!"), in: poke_channels) do |event|
+  poke_steps += 1
+  if poke_steps >= poke_requirement
+    poke_steps = 0
+    random_pokedex_num = (rand(809) + 1).to_s.rjust(3, '0')
+    pic_file = File.expand_path("pokemon_info/images/#{random_pokedex_num}.png", File.dirname(__FILE__))
+    event.send_file(File.open(pic_file, 'r'), caption: 'A wild Pokemon appeared! You can try to catch it with `p!catch` (not implemented)')
+  end
+end
+
 bot.run
