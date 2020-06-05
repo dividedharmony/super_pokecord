437ea423f8e15d282d520f4e5cfca9a6f3ae13c9
David Harmon <dharmon202@gmail.com>
2020-06-05 11:20:31 -0400

    Lock p!wild command to developer-level permissions
    We don't want anyone cheating.

--- command.rb
+++ command.rb
@@ -8,13 +8,17 @@ bot = Discordrb::Commands::CommandBot.new(
   prefix: 'p!'
 )
 
+if ENV['DEVELOPER_ID'].present?
+  bot.set_user_permission(ENV['DEVELOPER_ID'], 5)
+end
+
 bot.command :start do |event|
   starter_file = File.expand_path('assets/starters.png', File.dirname(__FILE__))
   event.send_file(File.open(starter_file, 'r'), caption: 'Pick your pokemon')
   "You can select one with `p!start [pokemon name]` (not implemented)"
 end
 
-bot.command :wild do |event|
+bot.command(:wild, permission_level: 2) do |event|
   random_pokedex_num = (rand(809) + 1).to_s.rjust(3, '0')
   pic_file = File.expand_path("pokemon_info/images/#{random_pokedex_num}.png", File.dirname(__FILE__))
   event.send_file(File.open(pic_file, 'r'), caption: 'A wild Pokemon appeared! You can try to catch it with `p!catch` (not implemented)')
