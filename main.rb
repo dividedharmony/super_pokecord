e036848116b2a9a6f2a904fc3b64be5e62dd6efe
David Harmon <dharmon202@gmail.com>
2020-06-04 20:59:44 -0400

    Add "not implemented" messages to poke commands

--- main.rb
+++ main.rb
@@ -14,8 +14,56 @@ bot.message(with_text: 'p!ping') do |event|
   event.respond 'PONG'
 end
 
+bot.message(with_text: 'p!help') do |event|
+  event.respond 'This is the Pokecord bot! It is currently underdevelopment, so not all commands may function properly. Check back soon for updates!'
+end
+
 bot.message(with_text: 'p!bye') do |event|
   event.respond 'Shutting down now'
+end
+
+%w{
+  start
+  pick
+  pokemon
+  order
+  catch
+  hint
+  info
+  catch
+  pokedex
+  nickname
+  release
+  mega
+  shop
+  fav
+  addfav
+  removefav
+  select
+  moves
+  learn
+  replace
+  duel
+  accept
+  use
+  confirm
+  balance
+  bal
+  market
+  trade
+  p
+  cancel
+  daily
+  silence
+  redeem
+  invite
+  server
+  patreon
+  appeal
+}.each do |command|
+  bot.message(with_text: "p!#{command}") do |event|
+    event.respond "The #{command} has not been implemented yet. Check back soon for new features and updates!"
+  end
 end
 
 bot.run
