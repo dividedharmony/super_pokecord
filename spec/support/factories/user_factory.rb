d7f4b0dcc4f2504eb84cf311991007079a55b1f4
David Harmon <dharmon202@gmail.com>
2020-06-19 19:34:25 -0400

    Fix discord_id attribute of user testing factory
    This was a static value that would violate the unique constraint
    if you used the factory to spawn more than one user.

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 TestingFactory.define(:user) do |factory|
-  factory.discord_id { '123456789' }
+  factory.sequence(:discord_id) { |n| "#{n}12345" }
   factory.created_at { Time.now }
   factory.exp_per_step 50
 
