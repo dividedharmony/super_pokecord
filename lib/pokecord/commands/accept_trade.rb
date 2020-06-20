5af85cfdfa0e37360d550bb98c229fe9ffa29530
David Harmon <dharmon202@gmail.com>
2020-06-20 18:42:46 -0400

    Add Pokecord::Commands::AcceptTrade
    This will be used for the user_2 of a trade invitation to accept
    the invitation.

---
+++ lib/pokecord/commands/accept_trade.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+require_relative '../../../db/connection'
+require_relative '../../repositories/user_repo'
+require_relative '../../repositories/trade_repo'
+
+module Pokecord
+  module Commands
+    class AcceptTrade
+      include Dry::Monads[:result]
+      include Dry::Monads::Do.for(:call)
+
+      def initialize(discord_id)
+        @discord_id = discord_id
+        @user_repo = Repositories::PokemonRepo.new(
+          Db::Connection.registered_container
+        )
+        @trade_repo = Repositories::TradeRepo.new(
+          Db::Connection.registered_container
+        )
+      end
+
+      def call
+        user = yield get_user
+        trade = yield get_trade(user)
+        yield validate_trade(user, trade)
+
+        update_cmd = trade_repo.trades.by_pk(trade.id).command(:update)
+        trade = update_cmd.call(
+          user_2_accepted: true
+        )
+        Success(trade)
+      end
+
+      private
+
+      attr_reader :discord_id, :user_repo, :trade_repo
+
+      def get_user
+        user = user_repo.users.where(discord_id: discord_id).one
+        if user.nil?
+          Failure(I18n.t('user_needs_to_start'))
+        else
+          Success(user)
+        end
+      end
+
+      def get_trade(user)
+        trade = trade_repo.pending_trades(user.id).first
+        if trade.nil?
+          Failure(I18n.t('accept_trade.no_such_trade'))
+        else
+          Success(trade)
+        end
+      end
+
+      def validate_trade(user, trade)
+        if trade.user_1_id == user.id
+          Failure(I18n.t('accept_trade.only_user_2_can_accept'))
+        elsif trade.user_2_accepted
+          Failure(I18n.t('accept_trade.already_accepted'))
+        else
+          Success(trade)
+        end
+      end
+    end
+  end
+end
---
+++ spec/pokecord/commands/accept_trade_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require_relative '../../../lib/pokecord/commands/accept_trade'
+
+RSpec.describe Pokecord::Commands::AcceptTrade do
+  describe '#call' do
+    let(:trade_repo) do
+      Repositories::TradeRepo.new(
+        Db::Connection.registered_container
+      )
+    end
+    let(:discord_id) { '123456' }
+
+    subject { described_class.new(discord_id).call }
+
+    context 'if no user exists with that discord_id' do
+      it 'returns a failure monad' do
+        expect(subject).to be_failure
+        expect(subject.failure).to eq I18n.t('user_needs_to_start')
+      end
+    end
+
+    context 'if a user exists with that discord_id' do
+      let!(:user) { TestingFactory[:user, discord_id: '123456'] }
+
+      context 'if user has no pending trades' do
+        let!(:trade) do
+          TestingFactory[
+            :trade,
+            user_2_accepted: false,
+            user_2_id: user.id,
+            expires_at: Time.now - (5 * 60)
+          ]
+        end
+
+        it 'returns a failure monad' do
+          expect { subject }.not_to change {
+            trade_repo.trades.by_pk(trade.id).one.user_2_accepted
+          }.from(false)
+          expect(subject).to be_failure
+          expect(subject.failure).to eq I18n.t('accept_trade.no_such_trade')
+        end
+      end
+
+      context 'if user has one pending trade' do
+        context 'if user is the user_1 of the trade' do
+          let!(:trade) do
+            TestingFactory[
+              :trade,
+              user_2_accepted: false,
+              user_1_id: user.id,
+            ]
+          end
+
+          it 'returns a failure monad' do
+            expect { subject }.not_to change {
+              trade_repo.trades.by_pk(trade.id).one.user_2_accepted
+            }.from(false)
+            expect(subject).to be_failure
+            expect(subject.failure).to eq I18n.t('accept_trade.only_user_2_can_accept')
+          end
+        end
+
+        context 'if trade has already been accepted' do
+          let!(:trade) do
+            TestingFactory[
+              :trade,
+              user_2_accepted: true,
+              user_2_id: user.id,
+            ]
+          end
+
+          it 'returns a failure monad' do
+            expect { subject }.not_to change {
+              trade_repo.trades.by_pk(trade.id).one.user_2_accepted
+            }.from(true)
+            expect(subject).to be_failure
+            expect(subject.failure).to eq I18n.t('accept_trade.already_accepted')
+          end
+        end
+
+        context 'if trade is otherwise valid' do
+          let!(:trade) do
+            TestingFactory[
+              :trade,
+              user_2_id: user.id,
+            ]
+          end
+
+          it 'updates the trade to be accepted' do
+            expect { subject }.to change {
+              trade_repo.trades.by_pk(trade.id).one.user_2_accepted
+            }.from(false).to(true)
+            expect(subject).to be_success
+            expect(subject.value!.id).to eq(trade.id)
+          end
+        end
+      end
+    end
+  end
+end
