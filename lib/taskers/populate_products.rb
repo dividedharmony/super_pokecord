6267a464db823b6986309e5643c6f9896dd6a0a6
David Harmon <dharmon202@gmail.com>
2020-07-06 12:40:50 -0400

    Allow Taskers::PopulateProducts to update products as well
    This also adds price, page_num, and position number to visible
    products.

--- lib/taskers/populate_products.rb
+++ lib/taskers/populate_products.rb
@@ -14,13 +14,17 @@ module Taskers
 
     def call
       output.puts "Beginning to populate products..."
-      visible_product_names.each do |name|
-        output.puts "Creating visible #{name} product"
-        product_repo.create(name: name, visible: true)
-      end
-      invisible_product_names.each do |name|
-        output.puts "Creating invisible #{name} product"
-        product_repo.create(name: name, visible: false)
+      product_attributes.each do |product_attr|
+        existing_product = product_repo.products.where(name: product_attr[:name]).one
+        if existing_product.nil?
+          output.puts "Creating #{product_attr[:name]} product"
+          product_repo.create(product_attr)
+        else
+          output.puts "Updating #{existing_product.name} product"
+          product_attr.delete(:name)
+          update_cmd = product_repo.products.by_pk(existing_product.id).command(:update)
+          update_cmd.call(product_attr)
+        end
       end
       output.puts "Finished populating products!"
     end
@@ -29,46 +33,176 @@ module Taskers
 
     attr_reader :output, :product_repo
 
-    def visible_product_names
+    def product_attributes
       [
-        'Fire Stone',
-        'Water Stone',
-        'Thunder Stone',
-        'Leaf Stone',
-        'Moon Stone',
-        'Sun Stone',
-        'Shiny Stone',
-        'Dusk Stone',
-        'Dawn Stone',
-        'Ice Stone',
-        'Oval Stone',
-        'Friendship Bracelet'
-      ]
-    end
-
-    def invisible_product_names
-      [
-        "King's Rock",
-        'Powerful Magnet',
-        'Metal Coat',
-        'Protector',
-        "King's Scale",
-        'Electirizer',
-        'Magmarizer',
-        'Upgrade',
-        'Dubious Disc',
-        'Razor Fang',
-        'Prism Scale',
-        'Reaper Cloth',
-        'Deep Sea Tooth',
-        'Deep Sea Scale',
-        'Sachet',
-        'Whipped Dream',
-        'Meltan Candy',
-        'Tart Apple',
-        'Sweet Apple',
-        'Cracked Pot',
-        'Strawberry Sweet'
+        {
+          name: 'Fire Stone',
+          visible: true,
+          price: 25_000,
+          page_num: 1,
+          position: 1
+        },
+        {
+          name: 'Water Stone',
+          visible: true,
+          price: 25_000,
+          page_num: 1,
+          position: 2
+        },
+        {
+          name: 'Thunder Stone',
+          visible: true,
+          price: 25_000,
+          page_num: 1,
+          position: 3
+        },
+        {
+          name: 'Leaf Stone',
+          visible: true,
+          price: 25_000,
+          page_num: 1,
+          position: 4
+        },
+        {
+          name: 'Moon Stone',
+          visible: true,
+          price: 25_000,
+          page_num: 1,
+          position: 5
+        },
+        {
+          name: 'Sun Stone',
+          visible: true,
+          price: 32_000,
+          page_num: 1,
+          position: 6
+        },
+        {
+          name: 'Shiny Stone',
+          visible: true,
+          price: 32_000,
+          page_num: 1,
+          position: 7
+        },
+        {
+          name: 'Dusk Stone',
+          visible: true,
+          price: 32_000,
+          page_num: 1,
+          position: 8
+        },
+        {
+          name: 'Dawn Stone',
+          visible: true,
+          price: 32_000,
+          page_num: 1,
+          position: 9
+        },
+        {
+          name: 'Ice Stone',
+          visible: true,
+          price: 32_000,
+          page_num: 1,
+          position: 10
+        },
+        {
+          name: 'Oval Stone',
+          visible: true,
+          price: 32_000,
+          page_num: 1,
+          position: 11
+        },
+        {
+          name: 'Friendship Bracelet',
+          visible: true,
+          price: 40_000,
+          page_num: 1,
+          position: 12
+        },
+        {
+          name: "King's Rock",
+          visible: false,
+        },
+        {
+          name: 'Powerful Magnet',
+          visible: false,
+        },
+        {
+          name: 'Metal Coat',
+          visible: false,
+        },
+        {
+          name: 'Protector',
+          visible: false,
+        },
+        {
+          name: "King's Scale",
+          visible: false,
+        },
+        {
+          name: 'Electirizer',
+          visible: false,
+        },
+        {
+          name: 'Magmarizer',
+          visible: false,
+        },
+        {
+          name: 'Upgrade',
+          visible: false,
+        },
+        {
+          name: 'Dubious Disc',
+          visible: false,
+        },
+        {
+          name: 'Razor Fang',
+          visible: false,
+        },
+        {
+          name: 'Prism Scale',
+          visible: false,
+        },
+        {
+          name: 'Reaper Cloth',
+          visible: false,
+        },
+        {
+          name: 'Deep Sea Tooth',
+          visible: false,
+        },
+        {
+          name: 'Deep Sea Scale',
+          visible: false,
+        },
+        {
+          name: 'Sachet',
+          visible: false,
+        },
+        {
+          name: 'Whipped Dream',
+          visible: false,
+        },
+        {
+          name: 'Meltan Candy',
+          visible: false,
+        },
+        {
+          name: 'Tart Apple',
+          visible: false,
+        },
+        {
+          name: 'Sweet Apple',
+          visible: false,
+        },
+        {
+          name: 'Cracked Pot',
+          visible: false,
+        },
+        {
+          name: 'Strawberry Sweet',
+          visible: false,
+        },
       ]
     end
   end
--- spec/taskers/populate_products_spec.rb
+++ spec/taskers/populate_products_spec.rb
@@ -17,45 +17,70 @@ RSpec.describe Taskers::PopulateProducts do
       allow(mock_output).to receive(:puts).with(instance_of(String))
     end
 
-    it 'populates the fight_types' do
-      expect { subject }.to change {
-        product_repo.products.count
-      }.from(0).to(33)
-      expect(product_repo.products.to_a.map(&:name)).to contain_exactly(
-        'Fire Stone',
-        'Water Stone',
-        'Thunder Stone',
-        'Leaf Stone',
-        'Moon Stone',
-        'Sun Stone',
-        'Shiny Stone',
-        'Dusk Stone',
-        'Dawn Stone',
-        'Ice Stone',
-        'Oval Stone',
-        'Friendship Bracelet',
-        "King's Rock",
-        'Powerful Magnet',
-        'Metal Coat',
-        'Protector',
-        "King's Scale",
-        'Electirizer',
-        'Magmarizer',
-        'Upgrade',
-        'Dubious Disc',
-        'Razor Fang',
-        'Prism Scale',
-        'Reaper Cloth',
-        'Deep Sea Tooth',
-        'Deep Sea Scale',
-        'Sachet',
-        'Whipped Dream',
-        'Meltan Candy',
-        'Tart Apple',
-        'Sweet Apple',
-        'Cracked Pot',
-        'Strawberry Sweet'
-      )
+    context 'if a product listed already exists' do
+      let!(:product) do
+        TestingFactory[
+          :product,
+          name: 'Water Stone',
+          visible: false,
+          price: 12,
+          page_num: 18,
+          position: 209
+        ]
+      end
+
+      it 'updates the product with the new attributes' do
+        expect { subject }.to change {
+          product_repo.products.by_pk(product.id).one.visible
+        }.from(false).to(true)
+        reloaded_product = product_repo.products.by_pk(product.id).one
+        expect(reloaded_product.price).to eq(25_000)
+        expect(reloaded_product.page_num).to eq(1)
+        expect(reloaded_product.position).to eq(2)
+      end
+    end
+
+    context 'if no products listed already exists' do
+      it 'populates the fight_types' do
+        expect { subject }.to change {
+          product_repo.products.count
+        }.from(0).to(33)
+        expect(product_repo.products.to_a.map(&:name)).to contain_exactly(
+          'Fire Stone',
+          'Water Stone',
+          'Thunder Stone',
+          'Leaf Stone',
+          'Moon Stone',
+          'Sun Stone',
+          'Shiny Stone',
+          'Dusk Stone',
+          'Dawn Stone',
+          'Ice Stone',
+          'Oval Stone',
+          'Friendship Bracelet',
+          "King's Rock",
+          'Powerful Magnet',
+          'Metal Coat',
+          'Protector',
+          "King's Scale",
+          'Electirizer',
+          'Magmarizer',
+          'Upgrade',
+          'Dubious Disc',
+          'Razor Fang',
+          'Prism Scale',
+          'Reaper Cloth',
+          'Deep Sea Tooth',
+          'Deep Sea Scale',
+          'Sachet',
+          'Whipped Dream',
+          'Meltan Candy',
+          'Tart Apple',
+          'Sweet Apple',
+          'Cracked Pot',
+          'Strawberry Sweet'
+        )
+      end
     end
   end
 end
