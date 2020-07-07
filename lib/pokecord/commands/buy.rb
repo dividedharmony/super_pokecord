3af4a672cb553e724aaaf6bc77ab5c5fe8d49ac1
David Harmon <dharmon202@gmail.com>
2020-07-07 14:41:10 -0400

    Refactor Buy command to use BaseInventoryCommand

--- lib/pokecord/commands/buy.rb
+++ lib/pokecord/commands/buy.rb
@@ -2,27 +2,16 @@
 
 require 'dry/monads/do'
 
-require_relative '../../../db/connection'
-require_relative '../../repositories/product_repo'
-require_relative '../../repositories/inventory_item_repo'
-
-require_relative './base_user_command'
+require_relative './base_inventory_command'
 
 module Pokecord
   module Commands
-    class Buy < BaseUserCommand
+    class Buy < BaseInventoryCommand
       include Dry::Monads::Do.for(:call)
 
       def initialize(discord_id, product_name, purchase_amount)
-        @product_name = product_name
         @purchase_amount = purchase_amount
-        @product_repo = Repositories::ProductRepo.new(
-          Db::Connection.registered_container
-        )
-        @inventory_repo = Repositories::InventoryItemRepo.new(
-          Db::Connection.registered_container
-        )
-        super(discord_id)
+        super(discord_id, product_name)
       end
 
       def call
@@ -39,17 +28,7 @@ module Pokecord
 
       private
 
-      attr_reader :product_name, :purchase_amount, :product_repo, :inventory_repo
-
-      def get_product
-        local_name_var = product_name
-        products = product_repo.products.where { visible.is(true) & name.ilike(local_name_var) }
-        if products.to_a.none?
-          Failure(I18n.t('buy.no_such_product', product_name: product_name))
-        else
-          Success(products.first)
-        end
-      end
+      attr_reader :purchase_amount
 
       def validate_can_purchase(user, product)
         if purchase_amount < 1
@@ -59,16 +38,6 @@ module Pokecord
         else
           Success(product)
         end
-      end
-
-      def find_or_create_inventory_item(user, product)
-        item = inventory_repo.inventory_items.where(user_id: user.id, product_id: product.id).first
-        item || inventory_repo.create(
-          user_id: user.id,
-          product_id: product.id,
-          created_at: Time.now,
-          updated_at: Time.now
-        )
       end
     end
   end
