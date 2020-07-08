440d7a2423113c364130e58fc5fba5be3351b480
David Harmon <dharmon202@gmail.com>
2020-07-08 15:25:53 -0400

    Refactor Pokecord::Commands::ListPokemon to use BaseUserCommand
    This also adds some tests for this class.

--- lib/pokecord/commands/list_pokemons.rb
+++ lib/pokecord/commands/list_pokemons.rb
@@ -1,43 +1,59 @@
 # frozen_string_literal: true
 
-require_relative '../../../db/connection'
-require_relative '../../repositories/user_repo'
+require 'dry/monads/do'
+
+require_relative './base_user_command'
 require_relative '../../repositories/spawned_pokemon_repo'
 
 module Pokecord
   module Commands
-    class ListPokemons
+    class ListPokemons < BaseUserCommand
+      ListPayload = Struct.new(:spawned_pokemons, :total_pages)
       PER_PAGE = 25
 
-      def initialize(discord_id, page_num)
-        @page_num = page_num
-        @user_repo = Repositories::UserRepo.new(
-          Db::Connection.registered_container
-        )
+      include Dry::Monads::Do.for(:call)
+
+      def initialize(discord_id, page_offset)
+        @page_offset = page_offset
         @spawn_repo = Repositories::SpawnedPokemonRepo.new(
           Db::Connection.registered_container
         )
-        @user = user_repo.users.where(discord_id: discord_id).one!
+        super(discord_id)
       end
 
-      def to_a
-        offset = page_num * PER_PAGE
-        spawn_repo.
+      def call
+        user = yield get_user
+        spawns = yield get_spawns(user)
+
+        Success(ListPayload.new(spawns, total_pages(user)))
+      end
+
+      private
+
+      attr_reader :page_offset, :spawn_repo
+
+      def offset
+        offset = page_offset * PER_PAGE
+      end
+
+      def get_spawns(user)
+        spawned_pokemons = spawn_repo.
           spawned_pokemons.
           combine(:pokemon).
           where(user_id: user.id).
           limit(PER_PAGE).
           offset(offset).
           to_a
+        if spawned_pokemons.none?
+          Failure(I18n.t('list_pokemon.no_pokemon_found'))
+        else
+          Success(spawned_pokemons)
+        end
       end
 
-      def total_pages
+      def total_pages(user)
         (spawn_repo.spawned_pokemons.where(user_id: user.id).count.to_f / PER_PAGE).ceil
       end
-
-      private
-
-      attr_reader :page_num, :user_repo, :spawn_repo, :user
     end
   end
 end
---
+++ spec/pokecord/commands/list_pokemons_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require_relative '../../../lib/pokecord/commands/list_pokemons'
+
+RSpec.describe Pokecord::Commands::ListPokemons do
+  it_behaves_like 'a user command' do
+    let(:command) { described_class.new('12345', 1) }
+  end
+
+  describe '#call' do
+    let!(:user) do
+      TestingFactory[
+        :user,
+        discord_id: '24680'
+      ]
+    end
+    let(:page_offset) { 0 }
+
+    subject { described_class.new('24680', page_offset).call }
+
+    context 'if user has no spawns' do
+      it 'returns a failure monad' do
+        expect(subject).to be_failure
+        expect(subject.failure).to eq(
+          I18n.t('list_pokemon.no_pokemon_found')
+        )
+      end
+    end
+
+    context 'if user has spawns' do
+      context 'if page_offset is 0' do
+        let!(:spawn) { TestingFactory[:spawned_pokemon, user_id: user.id] }
+
+        it 'returns a list payload wrapped in a success monad' do
+          expect(subject).to be_success
+          payload = subject.value!
+          expect(payload.spawned_pokemons.length).to eq(1)
+          expect(payload.spawned_pokemons.first.id).to eq(spawn.id)
+          expect(payload.total_pages).to eq(1)
+        end
+      end
+
+      context 'if page_offset is greater than 0' do
+        let(:page_offset) { 1 }
+
+        before do
+          51.times do |n|
+            TestingFactory[
+              :spawned_pokemon,
+              user_id: user.id,
+              nickname: "Name #{n + 1}"
+            ]
+          end
+        end
+
+        it 'returns a list payload offset by the page number wrapped in a success monad' do
+          expect(subject).to be_success
+          payload = subject.value!
+          expect(payload.spawned_pokemons.length).to eq(25)
+          expect(payload.spawned_pokemons.map(&:nickname)).to match_array(
+            (26..50).map { |n| "Name #{n}" }
+          )
+          expect(payload.total_pages).to eq(3)
+        end
+      end
+    end
+  end
+end
