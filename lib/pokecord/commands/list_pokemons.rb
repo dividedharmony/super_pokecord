98dc145c10741f599e9c58b35b228cba0e208594
David Harmon <dharmon202@gmail.com>
2020-07-08 16:23:10 -0400

    Allow ListPokemons to filter on favorite
    If only_favorite is set to true, it will only return favorited
    spawns.

--- lib/pokecord/commands/list_pokemons.rb
+++ lib/pokecord/commands/list_pokemons.rb
@@ -8,13 +8,14 @@ require_relative '../../repositories/spawned_pokemon_repo'
 module Pokecord
   module Commands
     class ListPokemons < BaseUserCommand
-      ListPayload = Struct.new(:spawned_pokemons, :total_pages)
+      ListPayload = Struct.new(:spawned_pokemons, :page_number, :total_pages)
       PER_PAGE = 25
 
       include Dry::Monads::Do.for(:call)
 
-      def initialize(discord_id, page_offset)
+      def initialize(discord_id, page_offset, only_favorites = false)
         @page_offset = page_offset
+        @only_favorites = only_favorites
         @spawn_repo = Repositories::SpawnedPokemonRepo.new(
           Db::Connection.registered_container
         )
@@ -25,29 +26,38 @@ module Pokecord
         user = yield get_user
         spawns = yield get_spawns(user)
 
-        Success(ListPayload.new(spawns, total_pages(user)))
+        Success(
+          ListPayload.new(
+            spawns,
+            page_offset + 1,
+            total_pages(user)
+          )
+        )
       end
 
       private
 
-      attr_reader :page_offset, :spawn_repo
+      attr_reader :page_offset, :only_favorites, :spawn_repo
 
       def offset
         offset = page_offset * PER_PAGE
       end
 
       def get_spawns(user)
-        spawned_pokemons = spawn_repo.
-          spawned_pokemons.
+        spawns_by_user = only_favorites ?
+          spawn_repo.favorited_by(user) :
+          spawn_repo.by_user(user)
+
+        limited_spawns = spawns_by_user.
           combine(:pokemon).
-          where(user_id: user.id).
           limit(PER_PAGE).
           offset(offset).
           to_a
-        if spawned_pokemons.none?
+
+        if limited_spawns.none?
           Failure(I18n.t('list_pokemon.no_pokemon_found'))
         else
-          Success(spawned_pokemons)
+          Success(limited_spawns)
         end
       end
 
--- spec/pokecord/commands/list_pokemons_spec.rb
+++ spec/pokecord/commands/list_pokemons_spec.rb
@@ -16,51 +16,97 @@ RSpec.describe Pokecord::Commands::ListPokemons do
     end
     let(:page_offset) { 0 }
 
-    subject { described_class.new('24680', page_offset).call }
+    subject { described_class.new('24680', page_offset, only_favorites).call }
 
-    context 'if user has no spawns' do
-      it 'returns a failure monad' do
-        expect(subject).to be_failure
-        expect(subject.failure).to eq(
-          I18n.t('list_pokemon.no_pokemon_found')
-        )
+    context 'if only_favorites is false' do
+      let(:only_favorites) { false }
+
+      context 'if user has no spawns' do
+        it 'returns a failure monad' do
+          expect(subject).to be_failure
+          expect(subject.failure).to eq(
+            I18n.t('list_pokemon.no_pokemon_found')
+          )
+        end
+      end
+
+      context 'if user has spawns' do
+        context 'if page_offset is 0' do
+          let!(:spawn) { TestingFactory[:spawned_pokemon, user_id: user.id] }
+
+          it 'returns a list payload wrapped in a success monad' do
+            expect(subject).to be_success
+            payload = subject.value!
+            expect(payload.spawned_pokemons.length).to eq(1)
+            expect(payload.spawned_pokemons.first.id).to eq(spawn.id)
+            expect(payload.page_number).to eq(1)
+            expect(payload.total_pages).to eq(1)
+          end
+        end
+
+        context 'if page_offset is greater than 0' do
+          let(:page_offset) { 1 }
+
+          before do
+            51.times do |n|
+              TestingFactory[
+                :spawned_pokemon,
+                user_id: user.id,
+                nickname: "Name #{n + 1}"
+              ]
+            end
+          end
+
+          it 'returns a list payload offset by the page number wrapped in a success monad' do
+            expect(subject).to be_success
+            payload = subject.value!
+            expect(payload.spawned_pokemons.length).to eq(25)
+            expect(payload.spawned_pokemons.map(&:nickname)).to match_array(
+              (26..50).map { |n| "Name #{n}" }
+            )
+            expect(payload.page_number).to eq(2)
+            expect(payload.total_pages).to eq(3)
+          end
+        end
       end
     end
 
-    context 'if user has spawns' do
-      context 'if page_offset is 0' do
-        let!(:spawn) { TestingFactory[:spawned_pokemon, user_id: user.id] }
+    context 'if only_favorites is true' do
+      let(:only_favorites) { true }
+
+      context 'if user has no favorite spawns' do
+        before do
+          TestingFactory[
+            :spawned_pokemon,
+            user_id: user.id,
+            favorite: false
+          ]
+        end
+
+        it 'returns a failure monad' do
+          expect(subject).to be_failure
+          expect(subject.failure).to eq(
+            I18n.t('list_pokemon.no_pokemon_found')
+          )
+        end
+      end
+
+      context 'if user has favorite spawns' do
+        let!(:spawn) do
+          TestingFactory[
+            :spawned_pokemon,
+            user_id: user.id,
+            favorite: true
+          ]
+        end
 
         it 'returns a list payload wrapped in a success monad' do
           expect(subject).to be_success
           payload = subject.value!
           expect(payload.spawned_pokemons.length).to eq(1)
           expect(payload.spawned_pokemons.first.id).to eq(spawn.id)
+          expect(payload.page_number).to eq(1)
           expect(payload.total_pages).to eq(1)
-        end
-      end
-
-      context 'if page_offset is greater than 0' do
-        let(:page_offset) { 1 }
-
-        before do
-          51.times do |n|
-            TestingFactory[
-              :spawned_pokemon,
-              user_id: user.id,
-              nickname: "Name #{n + 1}"
-            ]
-          end
-        end
-
-        it 'returns a list payload offset by the page number wrapped in a success monad' do
-          expect(subject).to be_success
-          payload = subject.value!
-          expect(payload.spawned_pokemons.length).to eq(25)
-          expect(payload.spawned_pokemons.map(&:nickname)).to match_array(
-            (26..50).map { |n| "Name #{n}" }
-          )
-          expect(payload.total_pages).to eq(3)
         end
       end
     end
