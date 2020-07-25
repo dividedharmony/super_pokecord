befc9b7bcedc07dd740c58bf2d7540ec55ee209a
David Harmon <dharmon202@gmail.com>
2020-07-24 22:25:22 -0400

    Refactor p!info to use command, payload, and embed_template
    This is the latest and greatest techniques for user commands.

--- command.rb
+++ command.rb
@@ -31,6 +31,7 @@ require_relative './lib/dnd/commands/assign_party_role'
 # admin commands
 require_relative './lib/pokecord/commands/admin/reset_balances'
 # embed templates
+require_relative './lib/pokecord/embed_templates/info'
 require_relative './lib/pokecord/embed_templates/trade'
 require_relative './lib/pokecord/embed_templates/shop_landing_page'
 require_relative './lib/pokecord/embed_templates/shop_items_page'
@@ -186,26 +187,12 @@ bot.command(:select) do |event, catch_number|
 end
 
 bot.command(:info) do |event|
-  info_cmd = Pokecord::Commands::Info.new(event.user.id.to_s)
-  poke_spawn = info_cmd.call
-  if poke_spawn.nil?
-    "You do not have a Pokemon selected to see its info. Use `p!pokemon` to view all your Pokemon and `p!select` to choose which one you want to name."
-  else
-    poke_ideal = poke_spawn.pokemon
-    pokedex_display = poke_ideal.pokedex_number.to_s.rjust(3, '0')
-    event.channel.send_embed do |embed|
-      embed.color = Pokecord::EmbedTemplates::EMBED_COLOR
-      embed.title = "#{event.user.name}'s #{poke_spawn.nickname || poke_ideal.name}"
-      embed.description = "Level #{poke_spawn.level} #{poke_ideal.name}"
-      embed.add_field(name: 'Pokedex No.', value: pokedex_display)
-      embed.add_field(name: 'HP', value: poke_ideal.base_hp)
-      embed.add_field(name: 'Attack', value: poke_ideal.base_attack)
-      embed.add_field(name: 'Defense', value: poke_ideal.base_defense)
-      embed.add_field(name: 'Sp. Attack', value: poke_ideal.base_sp_attack)
-      embed.add_field(name: 'Sp. Defense', value: poke_ideal.base_sp_defense)
-      embed.add_field(name: 'Speed', value: poke_ideal.base_speed)
-      embed.add_field(name: 'Caught At', value: poke_spawn.caught_at.strftime('%Y-%m-%d'))
-    end
+  info_result = Pokecord::Commands::Info.new(event.user.id.to_s).call
+  if info_result.success?
+    info_payload = info_result.value!
+    embed = Pokecord::EmbedTemplates::Info.new(event.user.name, info_payload).to_embed
+    event.channel.send_embed('', embed)
+    pokedex_display = info_payload.pokemon.stylized_pokedex_number
     event.send_file(
       File.open(
         File.expand_path(
@@ -214,6 +201,8 @@ bot.command(:info) do |event|
         'r'
       )
     )
+  else
+    info_result.failure
   end
 end
 
